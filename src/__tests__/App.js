aef64e6b18aaa23322b966d559e596297a518eee
Jesper Orb <jesper.orb@zocom.se>
2018-01-15 19:35:37 +0100

    Init

---
+++ src/__tests__/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { mount, shallow } from 'enzyme';
+import App from '../components/App';
+
+it('should render <App /> without user', () => {
+
+});
+
+it('should render <App /> with user', () => {
+
+});
+
+it('call the internal method loginSuccessful', () => {
+
+});
+
+it('call the internal method logout', () => {
+
+});
